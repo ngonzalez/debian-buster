027baa4d6b34137002605ec4d9d9bf914c130b3f
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2021-04-22 21:31:09 +0200

    Update README, config

--- config.sh
+++ config.sh
@@ -1,7 +1,4 @@
 # docker
-export IMAGE_TAG='debian-buster'
+export IMAGE_TAG='ngonzalez121/debian-buster'
 export USER='debian'
 export HOST_KEY=$(cat ~/.ssh/id_rsa.pub)
-
-# gcp
-export PROJECT_NAME='project'
